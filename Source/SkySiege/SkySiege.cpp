14a183a6a40bc27d4426b602e436d2fc8e172864
loochlabs <thecalooch@gmail.com>
2024-05-19 07:19:01 +0200

    initial commit

---
+++ Source/SkySiege/SkySiege.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "SkySiege.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, SkySiege, "SkySiege" );
